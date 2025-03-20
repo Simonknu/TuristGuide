0eaedaeb36825f68dd2295f6ed4b1b51aad19e0e
Simonknu <sikn0002@stud.kea.dk>
2025-03-20 17:33:36 +0100

    disabled test

--- src/test/java/com/example/turistguide/TuristGuideApplicationTests.java
+++ src/test/java/com/example/turistguide/TuristGuideApplicationTests.java
@@ -1,12 +1,16 @@
 package com.example.turistguide;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class TuristGuideApplicationTests {
 
     @Test
+    @Disabled
     void contextLoads() {
     }
 
